63dedd9b41b4f6b17e155c4d6849ebb362624a1c
Michael Singh <singh.michaelanthony@gmail.com>
2020-08-03 20:29:36 -0400

    add site metadata to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Video Game Armory Shop`,
+    description: `Find all the rare and even legendary items contained throughout the video gaming universe under one roof.`,
+    author: `@singhmi4`,
+  },
   plugins: [],
 }
